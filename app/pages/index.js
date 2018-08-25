8dcec8396c5d3555a13b12a4e923c88e729cc686
Marco Aurélio Prado <marco.pdsv@gmail.com>
2018-08-25 16:14:40 -0300

    Changing home page text

--- app/pages/index.js
+++ app/pages/index.js
@@ -27,7 +27,7 @@ const HomepageHeading = ({ mobile }) => (
     <Container text>
       <Header
         as='h1'
-        content='Seguro Automotivo'
+        content='Seguro Automotivooo'
         inverted
         style={{
           fontSize: mobile ? '2em' : '4em',
